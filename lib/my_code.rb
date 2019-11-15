ff3ffea59d26938f26db244195abfa0f1d86fd3c
Zeshan Raja <zraja4402@gmail.com>
2019-11-15 05:11:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,15 @@
-# Your Code Here
+def map(array)
+result = []
+i = 0 
+
+while i < array.length do 
+ result.push(yield(array[i]))
+
+ i += 1 
+ end
+result 
+end
+
+def reduce(array, starting_point)
+  
+end 
